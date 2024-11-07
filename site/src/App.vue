af9fefd7d155c9d746e7b2cb7153f914323336d7
Jos <git@liblisa.nl>
2024-11-07 13:44:12 +0100

    Increase text contrast

--- site/src/App.vue
+++ site/src/App.vue
@@ -33,7 +33,7 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000;
 
   display: flex;
   flex-direction: column;
